90101e1119f599c0b50ec46ce4fa697fb3bea02b
AschwinvanHulst <aschwin18@live.nl>
2019-04-13 19:35:15 +0200

    Driehoek klaar

--- src/Triangle.java
+++ src/Triangle.java
@@ -7,28 +7,41 @@ public class Triangle extends Shape {
     }
 
 
-
     @Override
     public void fill() {
-    boolean widthIsEven;
-    int pixelsInMiddle;
-    double pixelsGrowthPerLine;
-    double thisLineGrowthAsDouble;
-    int thisLineGrowthAsInt;
-    int numberOfPixels;
-    int numberOfEmpty;
+        boolean widthIsEven;
+        int pixelsInMiddle;
+        double pixelsGrowthPerLine;
+        double thisLineGrowthAsDouble;
+        int thisLineGrowthAsInt;
+        int numberOfPixels;
+        int numberOfEmpty;
 
-    widthIsEven = SIZEX % 2 == 0;
+        widthIsEven = SIZEX % 2 == 0;
 
-    if (widthIsEven) {
-        pixelsInMiddle = 2;
-    } else {
-        pixelsInMiddle = 1;
+        if (widthIsEven) {
+            pixelsInMiddle = 2;
+        } else {
+            pixelsInMiddle = 1;
+        }
+
+        pixelsGrowthPerLine = (double) (SIZEX - pixelsInMiddle) / (SIZEY - 1);
+
+        for (int lineNumber = 1; lineNumber <= SIZEY; lineNumber++) {
+
+            thisLineGrowthAsDouble = (lineNumber - 1) * pixelsGrowthPerLine;
+
+            thisLineGrowthAsInt = 2 * (int) (thisLineGrowthAsDouble / 2);
+            numberOfPixels = pixelsInMiddle + thisLineGrowthAsInt;
+
+            numberOfEmpty = (SIZEX - numberOfPixels) / 2;
+
+            for (int counter = 1; counter <= numberOfPixels; counter++) {
+                fillPixel(numberOfEmpty + counter - 1, SIZEY - lineNumber);
+            }
+        }
     }
-
-    pixelsGrowthPerLine = (double) (SIZEX - pixelsInMiddle) / (SIZEY - 1);
-
-    for (int lineNumber = 1; lineNumber <= SIZEY; lineNumber++) {
+}
 
 //
 //                    for (int xPixel = 0; xPixel < SIZEX; xPixel++) {
@@ -84,8 +97,7 @@ public class Triangle extends Shape {
 
                     if (xPixel <= yPixel) {
                         clearPixel(xPixel, yPixel);*/
-                }
-            }
+
 
 
 
