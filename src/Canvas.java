8ff758497ab3a9191f4f0d9d863953f6a94e41ec
AschwinvanHulst <aschwin18@live.nl>
2019-04-14 19:55:56 +0200

    Added
    import javax.swing.JFrame;
    import java.awt.Canvas;
    import java.awt.Graphics;
    import java.awt.Color;

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,3 +1,7 @@
+import javax.swing.JFrame;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Color;
 public class Canvas extends PixelGrid {
     public Canvas(int SIZEX, int SIZEY) {
 
